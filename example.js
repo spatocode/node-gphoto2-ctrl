1f0f460c32087810d5fae5a0c362761cbe21e69f
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-04-19 15:24:26 +0100

    add example file

---
+++ example.js
@@ -0,0 +1,19 @@
+import Gphoto2 from ".";
+
+const gphoto2 = new Gphoto2("./gphoto2.exe")
+
+gphoto2.listConfig((data, err) => {
+    if(err) {
+        console.error(err)
+        return
+    }
+    console.log(data)
+})
+
+gphoto2.setConfig("/main/capturesettings/shutterspeed=30", (data, err) => {
+    if(err) {
+        console.error(err)
+        return
+    }
+    console.log(data)
+})
