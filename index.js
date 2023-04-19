33adc707d8653b4dfafdb4737025175e047c188c
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-04-19 14:52:45 +0100

    provide implementation of queuing the config command

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ export default class Gphoto2 {
             throw console.error("Unable to find gphoto2 binary");
         }
         this.gphoto2Binary = gphoto2Location || hasbin.sync("gphoto2")
+        this.configs = []
     }
 
     listCameras(callback) {
@@ -50,7 +51,27 @@ export default class Gphoto2 {
         process.stderr.on('data', (err) => callback(err))
     }
 
+    _queueConfigCommand(config) {
+        const configPath = config.split("=")
+        const _path = configPath[0]
+        const value = configPath[1]
+        const index = -1
+        this.configs.some((config, i) => {
+            index = config.path == _path ? i : -1
+            return config.path == _path
+        })
+        if(index == -1) {
+            this.configs.push({path: configPath[0], value: configs[1]})
+        }
+        else {
+            this.configs[index] = {path: _path, value: value}
+        }
+    }
     setConfig(config, callback) {
+        if(!config && !config.includes("=")) {
+            throw console.error("Missing proper config parameter")
+        }
+        this._queueConfigCommand(config)
         const process = spawn(this.gphoto2Binary, ['--set-config', `${config}`])
         process.stderr.on('data', (err) => callback(err))
     }
